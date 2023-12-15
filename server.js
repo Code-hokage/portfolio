f683f8865d9a094ba0f2b55d59c093cf554a40c8
sharawi <adnanmish30@gmail.com>
2023-12-15 21:41:50 +0000

    contact test1

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.post('/', (req, res) => {
     })
 })
 
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
-});
+// app.listen(PORT, () => {
+//     console.log(`server running on port ${PORT}`)
+// });
